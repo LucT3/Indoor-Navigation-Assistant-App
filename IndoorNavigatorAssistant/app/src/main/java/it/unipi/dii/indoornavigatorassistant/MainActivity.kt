7b4bf724ace172d8507cabd5a9f08efef4c53107
LucT3 <luc.tart3@gmail.com>
2023-05-10 13:07:41 +0200

    Beacon Scan and Monitor Test
    functions tested on the MainActivity and then moved on MonitorTest class. To test with permissions and check if works.

--- IndoorNavigatorAssistant/app/src/main/java/it/unipi/dii/indoornavigatorassistant/MainActivity.kt
+++ IndoorNavigatorAssistant/app/src/main/java/it/unipi/dii/indoornavigatorassistant/MainActivity.kt
@@ -1,17 +1,26 @@
 package it.unipi.dii.indoornavigatorassistant
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
+import com.kontakt.sdk.android.ble.connection.OnServiceReadyListener
+import com.kontakt.sdk.android.ble.manager.ProximityManager
+import com.kontakt.sdk.android.ble.manager.ProximityManagerFactory
+import com.kontakt.sdk.android.ble.manager.listeners.IBeaconListener
+import com.kontakt.sdk.android.ble.manager.listeners.simple.SimpleIBeaconListener
 import com.kontakt.sdk.android.common.KontaktSDK
+import com.kontakt.sdk.android.common.profile.IBeaconDevice
+import com.kontakt.sdk.android.common.profile.IBeaconRegion
+
 
 class MainActivity : ComponentActivity() {
+    private var proximityManager: ProximityManager? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // Initialize Bluetooth API
         KontaktSDK.initialize(this)
         setContentView(R.layout.activity_main)
-    }
 
-    
+    }
 
 }
