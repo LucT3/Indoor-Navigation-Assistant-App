6763e1eda0beb05e33b9fad7f45056426af03455
LucT3 <luc.tart3@gmail.com>
2023-05-10 13:07:41 +0200

    Beacon Scan and Monitor Test
    functions tested on the MainActivity and then moved on MonitorTest class. To test with permissions and check if works.

--- IndoorNavigatorAssistant/app/src/main/java/it/unipi/dii/indoornavigatorassistant/MainActivity.kt
+++ IndoorNavigatorAssistant/app/src/main/java/it/unipi/dii/indoornavigatorassistant/MainActivity.kt
@@ -3,9 +3,18 @@ package it.unipi.dii.indoornavigatorassistant
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
+import com.kontakt.sdk.android.ble.connection.OnServiceReadyListener
+import com.kontakt.sdk.android.ble.manager.ProximityManager
+import com.kontakt.sdk.android.ble.manager.ProximityManagerFactory
+import com.kontakt.sdk.android.ble.manager.listeners.IBeaconListener
+import com.kontakt.sdk.android.ble.manager.listeners.simple.SimpleIBeaconListener
 import com.kontakt.sdk.android.common.KontaktSDK
+import com.kontakt.sdk.android.common.profile.IBeaconDevice
+import com.kontakt.sdk.android.common.profile.IBeaconRegion
+
 
 class MainActivity : ComponentActivity() {
+    private var proximityManager: ProximityManager? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // Initialize Bluetooth API
@@ -13,4 +22,5 @@ class MainActivity : ComponentActivity() {
         setContentView(R.layout.activity_main)
         Log.i(this.localClassName, "ciao")
     }
+
 }
---
+++ IndoorNavigatorAssistant/app/src/main/java/it/unipi/dii/indoornavigatorassistant/MonitorTest.kt
@@ -0,0 +1,56 @@
+package it.unipi.dii.indoornavigatorassistant
+
+import android.R
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.kontakt.sdk.android.ble.manager.ProximityManager
+import com.kontakt.sdk.android.ble.manager.ProximityManagerFactory
+import com.kontakt.sdk.android.ble.manager.listeners.EddystoneListener
+import com.kontakt.sdk.android.ble.manager.listeners.IBeaconListener
+import com.kontakt.sdk.android.ble.manager.listeners.simple.SimpleEddystoneListener
+import com.kontakt.sdk.android.ble.manager.listeners.simple.SimpleIBeaconListener
+import com.kontakt.sdk.android.common.KontaktSDK
+import com.kontakt.sdk.android.common.profile.IBeaconDevice
+import com.kontakt.sdk.android.common.profile.IBeaconRegion
+import com.kontakt.sdk.android.common.profile.IEddystoneDevice
+import com.kontakt.sdk.android.common.profile.IEddystoneNamespace
+
+
+class MonitorTest : AppCompatActivity() {
+    private var proximityManager: ProximityManager? = null
+
+    protected override fun onStart() {
+        proximityManager = ProximityManagerFactory.create(this)
+        proximityManager?.setIBeaconListener(createIBeaconListener())
+        super.onStart()
+        startScanning()
+    }
+
+    protected override fun onStop() {
+        println("**STOP MONITOR**")
+        proximityManager!!.stopScanning()
+        super.onStop()
+    }
+
+    protected override fun onDestroy() {
+        proximityManager!!.disconnect()
+        proximityManager = null
+        super.onDestroy()
+    }
+
+    private fun startScanning() {
+        println("**START SCANNING**")
+        proximityManager!!.connect { proximityManager!!.startScanning() }
+    }
+
+    private fun createIBeaconListener(): IBeaconListener {
+        println("**CREATE BEACON LISTENER")
+        return object : SimpleIBeaconListener() {
+            override fun onIBeaconDiscovered(ibeacon: IBeaconDevice, region: IBeaconRegion) {
+                Log.i("Sample", "IBeacon discovered: $ibeacon")
+                println("**BEACON DISCOVERED**: ${ibeacon}")
+            }
+        }
+    }
+}
