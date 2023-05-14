99eebb69f1954939edb54b8048122d8e610fb903
Riccardo Sagramoni <50587373+RiccardoSagramoni@users.noreply.github.com>
2023-05-14 18:23:28 +0200

    Bug fixing
    Co-Authored-By: LucT3 <92722255+LucT3@users.noreply.github.com>

--- IndoorNavigatorAssistant/app/src/main/java/it/unipi/dii/indoornavigatorassistant/dao/NavigationInfoProvider.kt
+++ IndoorNavigatorAssistant/app/src/main/java/it/unipi/dii/indoornavigatorassistant/dao/NavigationInfoProvider.kt
@@ -1,14 +1,16 @@
 package it.unipi.dii.indoornavigatorassistant.dao
 
 import android.content.Context
+import android.util.Log
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import it.unipi.dii.indoornavigatorassistant.model.BLERegion
+import it.unipi.dii.indoornavigatorassistant.util.Constants
 import java.io.IOException
 
 class NavigationInfoProvider(context: Context) {
 
-    private lateinit var bleRegionMap: MutableMap<String, List<String>>
+    private var bleRegionMap: MutableMap<String, List<String>> = mutableMapOf()
 
     init {
         val mapper = jacksonObjectMapper()
@@ -22,9 +24,13 @@ class NavigationInfoProvider(context: Context) {
             throw RuntimeException(ex)
         }
 
+        Log.d(Constants.LOG_TAG, "json: $jsonString")
+
         val bleRegions: List<BLERegion> = mapper.readValue(jsonString)
+        Log.d(Constants.LOG_TAG, "ble regions: $bleRegions")
+
         for (region in bleRegions) {
-            bleRegionMap.put(region.id, region.pointOfInterests)
+            bleRegionMap[region.id] = region.pointOfInterests
         }
 
 
@@ -43,3 +49,13 @@ class NavigationInfoProvider(context: Context) {
     }
     
 }
+
+/*
+    val provider = NavigationInfoProvider(this)
+    val poi1 = provider.getBLERegionInfo("SJG9", "U9LQ")
+    val poi2 = provider.getBLERegionInfo("U9LQ", "SJG9")
+    assert(poi1 == poi2)
+    poi1?.forEach {
+        Log.d(Constants.LOG_TAG, it)
+    }
+ */
