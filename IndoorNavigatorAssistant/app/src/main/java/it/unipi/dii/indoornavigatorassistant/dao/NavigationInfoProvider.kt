ba7cb93f611e06a87eb1cb2a6fbd56f062a5b580
Riccardo Sagramoni <50587373+RiccardoSagramoni@users.noreply.github.com>
2023-05-14 17:15:50 +0200

    NavigationInfoProvider
    Co-Authored-By: LucT3 <92722255+LucT3@users.noreply.github.com>

---
+++ IndoorNavigatorAssistant/app/src/main/java/it/unipi/dii/indoornavigatorassistant/dao/NavigationInfoProvider.kt
@@ -0,0 +1,14 @@
+package it.unipi.dii.indoornavigatorassistant.dao
+
+object NavigationInfoProvider {
+    
+    private lateinit var bleRegionMap: String
+    
+    fun init() {
+        // parse JSON object
+    }
+    
+    fun getBLERegionInfo(beacon1: String, beacon2: String): List<String> {
+        return null;
+    }
+}
---
+++ IndoorNavigatorAssistant/app/src/main/java/it/unipi/dii/indoornavigatorassistant/model/BLERegion.kt
@@ -0,0 +1,4 @@
+package it.unipi.dii.indoornavigatorassistant.model
+
+data class BLERegion {
+}
