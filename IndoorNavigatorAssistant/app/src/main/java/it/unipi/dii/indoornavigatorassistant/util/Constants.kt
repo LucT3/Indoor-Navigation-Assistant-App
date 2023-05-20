c08d6fd12e9bee5a4e582ce8e6a55f74ebf825e2
LucT3 <luc.tart3@gmail.com>
2023-05-20 11:18:28 +0200

    update text views, app logo and constants

--- IndoorNavigatorAssistant/app/src/main/java/it/unipi/dii/indoornavigatorassistant/util/Constants.kt
+++ IndoorNavigatorAssistant/app/src/main/java/it/unipi/dii/indoornavigatorassistant/util/Constants.kt
@@ -4,4 +4,9 @@ object Constants {
     const val LOG_TAG = "MSSS"
     const val ENABLE_BLUETOOTH_REQUEST_CODE = 111
     const val REQUEST_ENABLE_LOCATION = 1001
+
+    //QR Code and Beacon Scanner Constants
+    const val QR_CODE_INFO_MESSAGE = "Qr Code Point of Interest: "
+    const val BEACON_INFO_MESSAGE = "Current Region Detected: "
+
 }
